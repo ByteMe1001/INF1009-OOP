529c199293fc18fa405cd7e571e7f2a324ba4b93
nadasgithub <146849880+nadasgithub@users.noreply.github.com>
2024-02-17 22:49:39 +0800

    not cofrimed Scene.java

--- desktop/src/com/mygdx/game/Scene.java
+++ desktop/src/com/mygdx/game/Scene.java
@@ -1,1 +1,25 @@
+package com.mygdx.game;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public abstract class Scene {
+    protected SceneManager sceneManager;
+    protected List<Entity> entities;
+
+    public Scene(SceneManager sceneManager) {
+        this.sceneManager = sceneManager;
+        entities = new ArrayList<>();
+    }
+
+    public abstract void create();
+    public abstract void update();
+    public abstract void render(SpriteBatch batch);
+    public abstract void dispose();
+
+    public void addEntity(Entity entity) {
+        entities.add(entity);
+    }
+}
 
