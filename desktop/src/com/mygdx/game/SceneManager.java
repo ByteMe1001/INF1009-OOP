585034701b79bc0dd4430aeebd1b8cd0a54803cf
nadasgithub <146849880+nadasgithub@users.noreply.github.com>
2024-02-17 22:39:58 +0800

    not confiremd SceneManager.java

--- desktop/src/com/mygdx/game/SceneManager.java
+++ desktop/src/com/mygdx/game/SceneManager.java
@@ -1,1 +1,39 @@
 // keeps track of the scenes in a game, allowing to switch between them
+// to be further edited :(
+
+package com.mygdx.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SceneManager {
+    private List<Scene> scenes;
+    private Scene currentScene;
+
+    public SceneManager() {
+        scenes = new ArrayList<>();
+    }
+
+    public void addScene(Scene scene) {
+        scenes.add(scene);
+    }
+
+    public void setStartingScene(Scene startingScene) {
+        currentScene = startingScene;
+    }
+
+    public Scene getCurrentScene() {
+        return currentScene;
+    }
+
+    public void setCurrentScene(Scene scene) {
+        currentScene = scene;
+    }
+
+    public void dispose() {
+        for (Scene scene : scenes) {
+            scene.dispose();
+        }
+    }
+}
+
