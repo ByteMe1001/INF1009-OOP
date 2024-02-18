1930bc327617850bfee8a38b35540f42333f49c0
xwlow <2301949@sit.singaporetech.edu.sg>
2024-02-18 15:47:16 +0800

    Update CollisionManager.java

--- desktop/src/com/mygdx/game/CollisionManager.java
+++ desktop/src/com/mygdx/game/CollisionManager.java
@@ -1,11 +1,19 @@
 package com.mygdx.game;
 
-abstract public class CollisionManager extends EntityManager implements iCollision{
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+ public abstract class CollisionManager extends EntityManager implements iCollision{
 
     float x, y;
     int width, height;
 
+
+
     public CollisionManager (float x, float y, int width, int height){
+
         this.x = x;
         this.y = y;
         this.width = width;
@@ -32,4 +40,18 @@ abstract public class CollisionManager extends EntityManager implements iCollisi
     public boolean collidesWith(CollisionManager rect){
         return x < rect.x + rect.width && y < rect.y + rect.height && x + width > rect.x && y + height > rect.y;
      }
+
+
+/*
+     private void detectCollisions(){
+         ListIterator<Entity> iterator = entity.ListIterator();
+         while(iterator.hasNext()){
+             Entity entity0 = iterator.next();
+             if(entity2.intersects(entity.getBoundingBox())){ //based on youtube tutorial, getBoundingBox returns a rectangle
+                 entity2.hit(entity0);
+                 iterator.remove();
+             }
+         }
+     }
+ */
 }
