c90735c0578f82545d8b2fa6d222566fde336474
SaladBoss <160210343+SaladBoss@users.noreply.github.com>
2024-02-18 11:43:27 +0800

    Update EntityManager.java

--- desktop/src/com/mygdx/game/EntityManager.java
+++ desktop/src/com/mygdx/game/EntityManager.java
@@ -1,80 +1,41 @@
-
-package com.mygdx.game;
-
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import java.util.Random;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-
 public class EntityManager {
-
-    private SpriteBatch batch;
-    private ShapeRenderer renderer;
-    private final int NUM_DROPS = 10;
-    private List<Entity> entityList;
-
-
-
-    public EntityManager() {
-        entityList = new ArrayList<>();
-        batch = new SpriteBatch();
-        renderer = new ShapeRenderer();
-        //initializeEntities();
+    private List<Entity> entities;
+    private ConflictManager conflictManager;
+    //Constructor with collisionManager as an instance variable
+    public EntityManager(ConflictManager conflictManager) {
+        entities = new ArrayList<>();
+        this.conflictManager = conflictManager;
     }
+    //Add
+    public void addEntity(Entity entity) {
+        entities.add(entity);
+    }
+    //Remove
+    public void removeEntity(Entity entity) {
+        entities.remove(entity);
+    }
+    //Iterates through entites and calls their update method
+    public void updateEntities() {
+        Iterator<Entity> iterator = entities.iterator();
+        while (iterator.hasNext()) {
+            Entity entity = iterator.next();
+            entity.update();
 
-//    private void initializeEntities() {
-//        Random random = new Random();
-//
-//        // Add drops to the list
-//        for (int i = 0; i < NUM_DROPS; i++) {
-//            int xPos = random.nextInt(600);
-//            int speed = random.nextInt(50) + 20;  // Adjust the range as needed
-//            entityList.add(new TextureObject("droplet.png", xPos, Gdx.graphics.getHeight(), speed, true));
-//
-//            // Add all the entities to the list
-//            entityList.add(new TextureObject("bucket.png", 200, 0, 300, false));
-//            entityList.add(new circle(300, 300, 300, 50, Color.RED));
-//            entityList.add(new triangle(400, 100, 300, 90, 75, Color.GREEN));
-//        }
-//    }
-//
-//    public void update() {
-//        for (Entity entity : entityList) {
-//            if (entity instanceof TextureObject) {
-//                TextureObject textureObject = (TextureObject) entity;
-//                if (textureObject.AIControlled()) {
-//                    textureObject.moveAIControlled();
-//                } else {
-//                    textureObject.moveUserControlled();
-//                }
-//            } else {
-//                // Handle other types of entities if needed
-//                entity.moveUserControlled();
-//            }
-//        }
-//    }
-//
-//    public void draw() {
-//        renderer.begin(ShapeRenderer.ShapeType.Filled);
-//        for (Entity entity : entityList) {
-//            entity.draw(renderer);
-//        }
-//        renderer.end();
-//
-//        batch.begin();
-//        for (Entity entity : entityList) {
-//            if (entity instanceof TextureObject) {
-//                ((TextureObject) entity).draw(batch);
-//            }
-//        }
-//        batch.end();
-//    }
-
-    public void dispose() {
-        batch.dispose();
+            if (entity.isDestroyed()) {
+                iterator.remove();
+            }
+        }
+        // Handle collisions using the ConflictManager
+        conflictManager.handleCollisions(entities);
+    }
+    //Rendering method
+    public void renderEntities(SpriteBatch spriteBatch, ShapeRenderer shapeRenderer) {
+        for (Entity entity : entities) {
+            entity.render(spriteBatch, shapeRenderer);
+        }
     }
 }
