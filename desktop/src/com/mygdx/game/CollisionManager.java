2fc7a9013d7e8024afaba6c7c908bbc8b2493198
xwlow <2301949@sit.singaporetech.edu.sg>
2024-02-18 11:00:24 +0800

    Update CollisionManager.java

--- desktop/src/com/mygdx/game/CollisionManager.java
+++ desktop/src/com/mygdx/game/CollisionManager.java
@@ -2,12 +2,17 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.sun.org.apache.xerces.internal.impl.dv.dtd.ENTITYDatatypeValidator;
 
 public class CollisionManager extends EntityManager implements iCollision{
 
+    private Entity entity;
+
     @Override
     public void enemyCollide() {
-
+        if(entity.x == entity.x) {
+            entity.destroy();
+        }
     }
 
     @Override
