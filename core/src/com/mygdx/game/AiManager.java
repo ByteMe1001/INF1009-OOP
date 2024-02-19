e0ac5f9765917d8019dc2ffb101ccce8d3088f21
Joshyua <69667628+Joshyua@users.noreply.github.com>
2024-02-19 16:41:57 +0800

    Update AiManager.java
    updated as of 19/2. Its working in my test env for now...
    
    -josh

--- core/src/com/mygdx/game/AiManager.java
+++ core/src/com/mygdx/game/AiManager.java
@@ -3,21 +3,20 @@ package com.mygdx.game;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AiManager implements iAiMovement{
+public class AiManager {
     private List<Entity> aiEntities; // Stores all AI-controlled entities
     private iAiMovement defaultMovementBehavior;
 
-    /*//SAVE THIS FOR ACTUAL IMPLEMENTATION NEXT TIME 
-     	public AiManager(float maxMoveDistance) {
+    //SAVE THIS FOR ACTUAL IMPLEMENTATION NEXT TIME 
+     public AiManager(float maxMoveDistance) {
         this.aiEntities = new ArrayList<>();
         this.defaultMovementBehavior = new AiMovement(maxMoveDistance);
-    } */
-
-    // Default constructor
-    public AiManager() {
+    } 
+    
+     	/* public AiManager() {
         this.aiEntities = new ArrayList<>();
-        //this.defaultMovementBehavior = new AiMovement();
-    }
+        this.defaultMovementBehavior = new AiMovement();
+    } */
 
     // Method to add an AI-controlled entity
     public void addAiEntity(Entity entity) {
@@ -30,9 +29,4 @@ public class AiManager implements iAiMovement{
         }
     }
 
-    public void move(Entity entity) {
-
-    }
-
 }
-
