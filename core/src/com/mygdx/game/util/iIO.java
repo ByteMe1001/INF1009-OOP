aa7c0cf5fc480a7de9214c0ca940087d3e3dcab9
Gabriel <yukkybo22@gmail.com>
2024-02-23 04:35:33 +0800

    IO add O

--- core/src/com/mygdx/game/util/iIO.java
+++ core/src/com/mygdx/game/util/iIO.java
@@ -3,6 +3,8 @@ package com.mygdx.game.util;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public interface iIO {
     // Default method to detect W, A, S, D keys
@@ -25,5 +27,39 @@ public interface iIO {
         }
 
         else return "FALSE";
+    }
+
+    // Method for arrow key
+    default String inputKey2() {
+
+        if (Gdx.input.isKeyPressed(Keys.UP)) {
+            return "UP";
+        }
+
+        else if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+            return "DOWN";
+        }
+
+        else if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+            return "LEFT";
+        }
+
+        else if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+            return "RIGHT";
+        }
+
+        else return "FALSE";
+    }
+
+    // Default method to draw text on a SpriteBatch using a BitmapFont
+    default void drawText(SpriteBatch batch, BitmapFont font, String text, float x, float y) {
+        font.draw(batch, text, x, y);
+    }
+
+    // Default method to draw text on a SpriteBatch using a BitmapFont with specified scale
+    default void drawText(SpriteBatch batch, BitmapFont font, String text, float x, float y, float scaleX, float scaleY) {
+        font.getData().setScale(scaleX, scaleY);
+        font.draw(batch, text, x, y);
+        font.getData().setScale(1, 1); // Reset scale to default
     }
 }
